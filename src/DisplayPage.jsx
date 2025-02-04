fb45d3538c5f2b2a5b49005988a02e79db2d880f
Naldoszs <adeyeyeniyi@yahoo.com>
2025-02-04 01:06:11 +0100

    finished project

--- src/DisplayPage.jsx
+++ src/DisplayPage.jsx
@@ -31,30 +31,36 @@ const DisplayPage = () => {
   return (
     <section className="flex flex-col padding-desktop max-lg:padding-tablet max-sm:padding-phone p-4">
       <div
-        className="flex w-full space-x-1 justify-between mb-4"
-        aria-label="buttons' wrapper"
+        aria-label="header & topic wrapper"
+        className="flex flex-col w-full h-auto sticky top-2 z-[1000px] backdrop-blur-lg"
       >
-        {["Users", "Posts", "Comments"].map((text, index) => (
-          <Button
-            key={index}
-            text={text}
-            isClicked={activeIndex === index}
-            onClick={() => {
-              setActiveIndex(index);
-              //toggle the type of request
-              setReqType(text.toLowerCase());
-            }}
-            hoverColor="hover:bg-blue-200"
-          />
+        <h3 className="flex- w-full justify-center items-center bg-black text-black bg-opacity-15 text-center text-3xl mb-6 font-bold rounded-sm">
+          JSON TYPICODE FETCH PROJECT
+        </h3>
+        <div
+          className="flex w-full space-x-1 justify-between mb-4"
+          aria-label="buttons' wrapper"
+        >
+          {["Users", "Posts", "Comments"].map((text, index) => (
+            <Button
+              key={index}
+              text={text}
+              isClicked={activeIndex === index}
+              onClick={() => {
+                setActiveIndex(index);
+                //toggle the type of request
+                setReqType(text.toLowerCase());
+              }}
+              hoverColor="hover:bg-blue-200"
+            />
+          ))}
+        </div>
+      </div>
+      <div className="flex flex-col space-y-2 w-full h-auto overflow-hidden">
+        {jsonData.map((obj) => (
+          <ListItem obj={obj} />
         ))}
       </div>
-      {jsonData.map((obj) => (
-        /*   <li className="mb-5" key={obj.id}>
-          {JSON.stringify(obj)}
-        </li> */
-        <ListItem obj={obj} />
-      ))}
-      <div>{/* render the list item here */}</div>
     </section>
   );
 };
--- src/ListItem.jsx
+++ src/ListItem.jsx
@@ -1,7 +1,7 @@
 const ListItem = ({ obj }) => {
   return (
     <li
-      className="mb-5 border-2 border-slate-100 flex p-4 rounded-sm"
+      className="mb-5 border-2 border-slate-100 flex p-4 rounded-sm flex-1 flex-wrap"
       key={obj.id}
     >
       {JSON.stringify(obj)}
